f4c54120c80a8e0c6a92204a573227c3ccecf1a7
yushan <yushan@intervest.lk>
2021-01-10 16:28:27 +0530

    feat: Add PropTypes for necessary components

--- src/UserList.js
+++ src/UserList.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from 'prop-types';
 import CountVisibilityBtn from "./CountVisibilityBtn";
 import UserItem from "./UserItem";
 
@@ -36,6 +37,10 @@ class UserList extends Component {
             </div>
         )
     }
+}
+
+UserList.PropTypes = {
+    users: PropTypes.array.isRequired
 }
 
 export default UserList;
