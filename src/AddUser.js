b649e21a562e2a9c97314065c48ae2dee300cf52
yushan <yushanwebdev@gmail.com>
2021-01-10 12:09:21 +0530

    feat: Complete the app

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,12 +1,69 @@
-import React, { Component } from 'react';
-import UserFields from './UserFields';
+import { Component } from 'react';
 
 class AddUser extends Component {
+    state = {
+        first: '',
+        last: '',
+        username: ''
+    }
+
+    onFirstChange = e => {
+        this.setState(prevState => ({
+            first: this.validatedVal(e.target.value)
+        }))
+    }
+
+    onLastChange = e => {
+        this.setState(prevState => ({
+            last: this.validatedVal(e.target.value)
+        }))
+    }
+
+    onUserNameChange = e => {
+        this.setState(prevState => ({
+            username: this.validatedVal(e.target.value)
+        }))
+    }
+
+    addUser = e => {
+        e.preventDefault();
+        if (!this.props.checkUserNameExists(this.state.username)) {
+            this.props.updateUsersList(this.state);
+            this.setState(prevState => ({
+                first: '',
+                last: '',
+                username: ''
+            }));
+        } else {
+            alert(`${this.state.username} exists.`);
+        }
+    }
+
+    checkSubmitEligiblity = ({ first, last, username }) => (
+        !(first !== '' && last !== '' && username !== '')
+    )
+
+    validatedVal = input => (
+        input !== '' ? input.trim() : ''
+    )
+
     render() {
-        return(
-            <form>
-                <UserFields />
-                <button type="submit">Add</button>
+        const { first, last, username } = this.state;
+        return (
+            <form onSubmit={this.addUser}>
+                <div className="form-control">
+                    <label htmlFor="first-name">First Name</label>
+                    <input type="text" name="first-name" id="first-name" onChange={this.onFirstChange} value={first} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="last-name">Last Name</label>
+                    <input type="text" name="last-name" id="last-name" onChange={this.onLastChange} value={last} />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" onChange={this.onUserNameChange} value={username} />
+                </div>
+                <button type="submit" disabled={this.checkSubmitEligiblity(this.state)}>Add</button>
             </form>
         )
     }
--- src/UserList.js
+++ src/UserList.js
@@ -1,16 +1,31 @@
 import { Component } from "react";
+import CountVisibilityBtn from "./CountVisibilityBtn";
+import UserItem from "./UserItem";
+
 
 class UserList extends Component {
+    state = {
+        showCount: true
+    }
+
+    changeCountVisiblity = () => {
+        this.setState(prevState => ({
+            showCount: !prevState.showCount
+        }))
+    }
+
     render() {
-        return(
+        return (
             <div>
+                <h2>Users</h2>
                 <ol>
                     {
-                        this.props.users.map((user) => (
-                            <li key={user.id}>{`${user.username} played ${user.count} games.`}</li>
+                        this.props.users.map(user => (
+                            this.state.showCount ? <UserItem key={user.id} username={user.username} count={user.count} /> : <UserItem key={user.id} username={user.username} count='\*' />
                         ))
                     }
                 </ol>
+                <CountVisibilityBtn showCount={this.state.showCount}  changeCountVisiblity={this.changeCountVisiblity} />
             </div>
         )
     }
