ea4bb6d2e6a0c4bda7a09cf1d9751f429ca4f118
yushan <yushanwebdev@gmail.com>
2021-01-10 16:28:27 +0530

    feat: Add PropTypes for necessary components

--- src/CountVisibilityBtn.js
+++ src/CountVisibilityBtn.js
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types';
+
 const CountVisibilityBtn = props => {
     const onBtnClick = e => {
         props.changeCountVisiblity();
     }
 
     return <button onClick={onBtnClick}>{props.showCount ? `Hide the Number of Games Played` : `Show the Number of Games Played`}</button>;
+}
+
+CountVisibilityBtn.PropTypes = {
+    showCount: PropTypes.bool.isRequired,
+    changeCountVisiblity: PropTypes.func.isRequired
 }
 
 export default CountVisibilityBtn;
